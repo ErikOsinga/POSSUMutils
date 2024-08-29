18b43128d0b5c963ddc5180fccac60b652107eb7
ErikOsinga <erikosinga876@hotmail.com>
2024-08-29 16:55:58 -0400

    only if new downloads run script;

--- downloadscript.py
+++ downloadscript.py
@@ -19,13 +19,17 @@ WORKFLOW:
 """
 
 import os
+import sys
 from prefect import flow, task
 # important to grab _run_remote() because run_remote() is wrapped in sys.exit()
 from possum2caom2.composable import _run_remote as possum_run_remote # type: ignore
 
 @task
 def launch_possum_run_remote():
-    """change workdir and launch download script"""
+    """change workdir and launch download script
+    
+    return number of succesfully downloaded+ingested tiles
+    """
 
     download_workdir = "/arc/projects/CIRADA/polarimetry/ASKAP/Tiles/downloads/"
     # Start possum_run in correct workdir
@@ -33,7 +37,20 @@ def launch_possum_run_remote():
     
     result = possum_run_remote()
 
-    return result
+    # Print the results to view them in Prefect
+    report_file = f"{download_workdir}/download_logs/downloads_report.txt"
+    
+    if not os.path.exists(report_file):
+        print(f"Report file {report_file} does not exist.")
+        return 0
+    
+    with open(report_file, 'r') as file:
+        for line in file:
+            print(line)
+            if "Number of Successes" in line:
+                successes = int(line.split(":")[1].strip())
+
+    return successes
 
 @task
 def launch_make_symlinks():
@@ -41,8 +58,11 @@ def launch_make_symlinks():
 
     tile_workdir = "/arc/projects/CIRADA/polarimetry/ASKAP/Tiles/"
     os.chdir(tile_workdir)
+
     # should be found in the tile_workdir
+    sys.path.append(tile_workdir)
     from create_symlinks import create_symlinks
+
     create_symlinks()
 
 @flow(log_prints=True)
@@ -54,10 +74,12 @@ def do_download():
 
     """
 
-    launch_possum_run_remote()
+    successes = launch_possum_run_remote()
 
-    launch_make_symlinks()
-
+    if successes > 0:
+        launch_make_symlinks()
+    else:
+        print("No sucessful tile downloads. Not running symlinks")
 
 if __name__ == "__main__":
     do_download()
