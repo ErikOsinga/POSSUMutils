914f004bd0cb2a5b47a60a972072106c4d41a12b
ErikOsinga <erikosinga876@hotmail.com>
2024-08-29 15:55:30 -0400

    pipeline for download from pawsey

---
+++ submit_download.py
@@ -0,0 +1,56 @@
+"""
+This script is called periodically from p1 with cron
+
+see 
+
+crontab -e
+
+
+It submits a download job to CANFAR which will pull new files off' the pawsey storage.
+
+TODO: what if job gets killed? 
+the particular flow run might show what happens?
+
+"""
+
+from skaha.session import Session
+session = Session()
+
+def launch_download():
+    """Launch tile download script"""
+
+    print("Launching tile download script on CANFAR")
+    run_name = "download"
+    
+    # optionally :latest for always the latest version
+    image = "images.canfar.net/cirada/possumpipelineprefect-3.12:latest"
+    # good default values for download script
+    cores = 2
+    ram = 16  # Check allowed values at canfar.net/science-portal
+
+    # Template bash/python script to run
+    cmd = "bash"
+    args = "/arc/projects/CIRADA/polarimetry/software/downloadscript.sh"
+
+    print("Launching session")
+    print(f"Command: {cmd} {args}")
+
+    session_id = session.create(
+        name=run_name.replace('_', '-'),  # Prevent Error 400: name can only contain alpha-numeric chars and '-'
+        image=image,
+        cores=cores,
+        ram=ram,
+        kind="headless",
+        cmd=cmd,
+        args=args,
+        replicas=1,
+    )
+
+    print("Check sessions at https://ws-uv.canfar.net/skaha/v0/session")
+    print(f"Check logs at https://ws-uv.canfar.net/skaha/v0/session/{session_id[0]}?view=logs")    
+
+    return
+
+if __name__ == "__main__":
+
+    launch_download()
