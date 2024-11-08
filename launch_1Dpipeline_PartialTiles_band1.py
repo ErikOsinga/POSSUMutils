e9dabbf3069e41e96c4bc5cdc4c1c5c8d6376d84
ErikOsinga <erikosinga876@hotmail.com>
2024-11-08 14:24:03 -0500

    attach SBid to pipeline launch

--- launch_1Dpipeline_PartialTiles_band1.py
+++ launch_1Dpipeline_PartialTiles_band1.py
@@ -9,11 +9,11 @@ from skaha.models import ContainerRegistry
 # session = Session(registry=registry)
 session = Session()
 
-def launch_session(run_name, field_ID, tilenumber, image, cores, ram):
+def launch_session(run_name, field_ID, tilenumber, SBnumber, image, cores, ram):
     """Launch 1D pipeline Partial Tile run"""
 
     # Template bash script to run
-    args = f"/arc/projects/CIRADA/polarimetry/software/run_1Dpipeline_PartialTiles_band1.sh {run_name} {field_ID} {tilenumber}"
+    args = f"/arc/projects/CIRADA/polarimetry/software/run_1Dpipeline_PartialTiles_band1.sh {run_name} {field_ID} {tilenumber} {SBnumber}"
 
     print("Launching session")
     print(f"Command: bash {args}")
@@ -39,10 +39,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Launch a 1D pipeline Partial Tiles run")
     parser.add_argument("field_ID", type=str, help="The field ID to process")
     parser.add_argument("tilenumber", type=int, help="The tile number to process")
+    parser.add_argument("SBnumber", type=int, help="The SB number to process")
 
     args = parser.parse_args()
     field_ID = args.field_ID
     tilenumber = args.tilenumber
+    SBnumber = args.SBnumber
+    
     # max 15 characters for run name
     run_name = f"{field_ID}_{tilenumber}"
 
@@ -50,7 +53,7 @@ if __name__ == "__main__":
     # image = "images.canfar.net/cirada/possumpipelineprefect-3.12:latest"
     image = "images.canfar.net/cirada/possumpipelineprefect-3.12:v1.11.0"
     # good default values
-    cores = 16
-    ram = 40  # Check allowed values at canfar.net/science-portal
+    cores = 4
+    ram = 4  # Check allowed values at canfar.net/science-portal
 
-    launch_session(run_name, field_ID, tilenumber, image, cores, ram)
+    launch_session(run_name, field_ID, tilenumber, SBnumber, image, cores, ram)
