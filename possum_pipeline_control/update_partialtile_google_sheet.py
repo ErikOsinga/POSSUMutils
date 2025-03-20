1c6e4c88085c7f6c1a593b657e2c07cb37deb331
ErikOsinga <erikosinga876@hotmail.com>
2025-03-20 17:11:05 -0400

    update possum 1D partial tile pipeline control from Cameron sheet

--- possum_pipeline_control/update_partialtile_google_sheet.py
+++ possum_pipeline_control/update_partialtile_google_sheet.py
@@ -9,13 +9,19 @@ import time
 import subprocess
 import astropy.table as at
 
+"""
+Run "check_status_and_launch_1Dpipeline_PartialTiles.py 'pre' " based on Camerons' POSSUM Pipeline Status google sheet.
+That will download the tiles in a CANFAR job and populate the google sheet.
+
+"""
+
 def get_ready_fields(band):
     """
     Connects to the POSSUM Status Monitor Google Sheet and returns a sub-table
     containing only the rows where the 'sbid' and 'aus_src' fields are not empty.
     
-    Uses the following Google API token:
-        /arc/home/ErikOsinga/.ssh/psm_gspread_token.json
+    Uses the following Google API token on p1:
+        /home/erik/.ssh/psm_gspread_token.json
     
     And the sheet URL:
         https://docs.google.com/spreadsheets/d/1sWCtxSSzTwjYjhxr1_KVLWG2AnrHwSJf_RWQow7wbH0
@@ -24,7 +30,7 @@ def get_ready_fields(band):
         '1' if band == '943MHz', otherwise '2'.
     """
     # POSSUM Status Monitor
-    Google_API_token = "/arc/home/ErikOsinga/.ssh/psm_gspread_token.json"
+    Google_API_token = "/home/erik/.ssh/psm_gspread_token.json"
     # Authenticate and grab the spreadsheet
     gc = gspread.service_account(filename=Google_API_token)
     ps = gc.open_by_url('https://docs.google.com/spreadsheets/d/1sWCtxSSzTwjYjhxr1_KVLWG2AnrHwSJf_RWQow7wbH0')
@@ -36,8 +42,8 @@ def get_ready_fields(band):
     column_names = tile_data[0]
     tile_table = at.Table(np.array(tile_data)[1:], names=column_names)
     
-    # Find all rows that have "sbid" not empty and "aus_src" not empty
-    mask = (tile_table['sbid'] != '') & (tile_table['aus_src'] != '')
+    # Find all rows that have "sbid" not empty and "aus_src" not empty and 'single_SB_1D_pipeline' empty
+    mask = (tile_table['sbid'] != '') & (tile_table['aus_src'] != '') & (tile_table['single_SB_1D_pipeline'] == '')
     ready_table = tile_table[mask]
     return ready_table
 
@@ -53,11 +59,11 @@ def launch_pipeline_command(fieldname, SBID):
     subprocess.run(command, shell=True, check=True)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Update Partial Tile Google Sheet")
-    parser.add_argument("band", choices=["943MHz", "1367MHz"], help="The frequency band of the tile", default="943MHz")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description="Update Partial Tile Google Sheet")
+    # parser.add_argument("band", choices=["943MHz", "1367MHz"], help="The frequency band of the tile")
+    # args = parser.parse_args()
     
-    ready_table = get_ready_fields(args.band)
+    ready_table = get_ready_fields("943MHz")
     
     # Loop over each row in the returned table to launch the pipeline command.
     # The 'fieldname' is taken from the column "name" with "EMU_" stripped if present.
@@ -68,5 +74,4 @@ if __name__ == "__main__":
             fieldname = fieldname[len("EMU_"):]
         SBID = row["sbid"]
         launch_pipeline_command(fieldname, SBID)
-        # Wait for 10 hours (10*3600 = 36000 seconds) between each call.
-        time.sleep(36000)
+        break # only do one every time the script is called
