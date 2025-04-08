2920f83e34901a905ccc985fd7c924a46d72002f
ErikOsinga <erikosinga876@hotmail.com>
2025-04-08 09:53:19 -0400

    remove spaces

--- cirada_software/collate_1Dpipeline_PartialTiles.sh
+++ cirada_software/collate_1Dpipeline_PartialTiles.sh
@@ -1,6 +1,6 @@
 # parse the argument basedir
 echo "Running collate job from directory $1"
-workdir = $1
+workdir=$1
 
 # for band 1, directory is also "943MHz"
 psrecord "python /arc/projects/CIRADA/polarimetry/software/POSSUM_Polarimetry_Pipeline/pipeline/pipeline_prefect.py config_collate_band1.ini" --include-children --log $workdir/psrecord.txt --plot $workdir/psrecord.png --interval 1
