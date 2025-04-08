86e8a5249f975a741f3ea8e2fd81ed86aab69ffd
ErikOsinga <erikosinga876@hotmail.com>
2025-04-08 09:55:36 -0400

    add ssh to p1

--- cirada_software/collate_1Dpipeline_PartialTiles.sh
+++ cirada_software/collate_1Dpipeline_PartialTiles.sh
@@ -2,5 +2,12 @@
 echo "Running collate job from directory $1"
 workdir=$1
 
+echo "Opening SSH tunnel to prefect server host (p1)"
+# open connection
+ssh -fNT -L 4200:localhost:4200 erik@206.12.93.32
+# set which port to communicate results to 
+export PREFECT_API_URL="http://localhost:4200/api"
+
+
 # for band 1, directory is also "943MHz"
 psrecord "python /arc/projects/CIRADA/polarimetry/software/POSSUM_Polarimetry_Pipeline/pipeline/pipeline_prefect.py config_collate_band1.ini" --include-children --log $workdir/psrecord.txt --plot $workdir/psrecord.png --interval 1
