d291ca47b9ceea28567d8009970c1024daa7e28a
ErikOsinga <erikosinga876@hotmail.com>
2025-04-08 10:00:29 -0400

    fix workdir

--- cirada_software/collate_1Dpipeline_PartialTiles.sh
+++ cirada_software/collate_1Dpipeline_PartialTiles.sh
@@ -1,6 +1,7 @@
 # parse the argument basedir
 echo "Running collate job from directory $1"
-workdir=$1
+workdir=$1/combined/
+# e.g. /arc/projects/CIRADA/polarimetry/pipeline_runs/partial_tiles/943MHz/
 
 echo "Opening SSH tunnel to prefect server host (p1)"
 # open connection
@@ -10,4 +11,4 @@ export PREFECT_API_URL="http://localhost:4200/api"
 
 
 # for band 1, directory is also "943MHz"
-psrecord "python /arc/projects/CIRADA/polarimetry/software/POSSUM_Polarimetry_Pipeline/pipeline/pipeline_prefect.py config_collate_band1.ini" --include-children --log $workdir/psrecord.txt --plot $workdir/psrecord.png --interval 1
+psrecord "python /arc/projects/CIRADA/polarimetry/software/POSSUM_Polarimetry_Pipeline/pipeline/pipeline_prefect.py $workdir/config_collate_band1.ini" --include-children --log $workdir/psrecord.txt --plot $workdir/psrecord.png --interval 1
