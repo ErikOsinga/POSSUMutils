0275bd562c9273abd8ff592dc3b0970d47413756
ErikOsinga <erikosinga876@hotmail.com>
2025-11-25 16:28:19 -0500

    move to the proper ingest script

--- cirada_software/ingest_3Dpipeline_band1_prefect.sh
+++ cirada_software/ingest_3Dpipeline_band1_prefect.sh
@@ -10,5 +10,7 @@ export PREFECT_API_URL="http://localhost:4200/api"
 echo "TEMPORARILY: adding RMtools[dev] to pythonpath until new release (>v1.4.6)"
 export PYTHONPATH="/arc/projects/CIRADA/polarimetry/software/RMtoolsdev/:$PYTHONPATH"
 
+# move to POSSUMutils base directory
+cd /arc/projects/CIRADA/polarimetry/software/POSSUMutils/
 # arguments: tile_number, band ("943MHz" or "1367MHz")
-python /arc/projects/CIRADA/polarimetry/software/ingest3Dpipeline.py $1 $2
+python -m possum_pipeline_control.ingest3Dpipeline $1 $2
