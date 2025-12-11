5424eb341d01a5041d74a14ec7a9886615907533
ErikOsinga <erikosinga876@hotmail.com>
2025-12-11 14:59:42 -0500

    add create symlinks script

---
+++ cirada_software/create_symlinks.sh
@@ -0,0 +1,16 @@
+
+
+
+# Move to the POSSUMutils directory
+cd /arc/projects/CIRADA/polarimetry/software/POSSUMutils
+
+# Create symbolic links from the timeblocked directories in the tiledir
+python -m cirada_software.create_symlinks --tiledir /arc/projects/CIRADA/polarimetry/ASKAP/Tiles
+
+# Fix duplicate downloads, downloads are found in the downloaddir, symbolic link logs in the logdir
+python -m cirada_software.fix_duplicate_downloads \
+  --downloaddir /arc/projects/CIRADA/polarimetry/ASKAP/Tiles/downloads \
+  --logdir /arc/projects/CIRADA/polarimetry/ASKAP/Tiles/symlink_logs/
+
+
+
