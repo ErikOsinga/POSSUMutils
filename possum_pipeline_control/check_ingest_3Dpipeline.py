f5e0f45ff126e6d54250f668441fb2358054a52c
ErikOsinga <erikosinga876@hotmail.com>
2025-12-17 15:40:39 -0500

    update to v1.16.0 container

--- possum_pipeline_control/check_ingest_3Dpipeline.py
+++ possum_pipeline_control/check_ingest_3Dpipeline.py
@@ -71,7 +71,7 @@ def launch_ingest(tilenumber, band):
     run_name = f"ingest{tilenumber}"
     # optionally :latest for always the latest version (SEEMS TO BE BROKEN)
     # image = "images.canfar.net/cirada/possumpipelineprefect-3.12:latest"
-    image = "images.canfar.net/cirada/possumpipelineprefect-3.12:v1.15.2"
+    image = "images.canfar.net/cirada/possumpipelineprefect-3.12:v1.16.0"
     # good default values for ingest script
     cores = 2
     ram = 32  # Check allowed values at canfar.net/science-portal
