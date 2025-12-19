cac2d2b71f84f205a00601ef71423183d082a9e2
riniangreani2 <rini.angreani@curtin.edu.au>
2025-12-19 18:05:08 +0800

    Make sure test db is correct before executing tests

--- automation/database_queries.py
+++ automation/database_queries.py
@@ -33,8 +33,20 @@ def get_database_parameters(test=False, database_config_path: str = "automation/
     """
     Get database parameters from env file (test.env for test, config.env otherwise)
     """
+    test_db_name = 'possum_test'
     if test:
         load_dotenv(dotenv_path='automation/unit_tests/test.env')
+        # Check that we're not accidentally connecting to prod DB in test mode
+        if (test_db_name != os.getenv('TEST_DATABASE_NAME')):
+            raise ConnectionError("""Please make sure your test database is named possum_test!
+                        This is to avoid accidentally using real prod database in tests.""")
+        return {
+            'dbname': os.getenv('TEST_DATABASE_NAME'),
+            'user': os.getenv('TEST_DATABASE_USER'),
+            'password': os.getenv('TEST_DATABASE_PASSWORD'),
+            'host': os.getenv('TEST_DATABASE_HOST'),
+            'port': os.getenv('TEST_DATABASE_PORT')
+        }
     else:
         print(f"Attempting to load database config from {database_config_path}")
 
@@ -46,13 +58,12 @@ def get_database_parameters(test=False, database_config_path: str = "automation/
         
         # Get database connection details from config.env file
         load_dotenv(dotenv_path=database_config_path)
-
-    return {
-        'dbname': os.getenv('DATABASE_NAME'),
-        'user': os.getenv('DATABASE_USER'),
-        'password': os.getenv('DATABASE_PASSWORD'),
-        'host': os.getenv('DATABASE_HOST'),
-        'port': os.getenv('DATABASE_PORT')
+        return {
+            'dbname': os.getenv('POSSUM_DATABASE_NAME'),
+            'user': os.getenv('POSSUM_DATABASE_USER'),
+            'password': os.getenv('POSSUM_DATABASE_PASSWORD'),
+            'host': os.getenv('POSSUM_DATABASE_HOST'),
+            'port': os.getenv('POSSUM_DATABASE_PORT')
     }
 
 def execute_update_query(query, conn, params=None, verbose=False):
