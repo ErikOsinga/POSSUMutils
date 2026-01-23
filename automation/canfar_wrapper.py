67dcf6a14651c46348e4df551505f00b7ab7394e
riniangreani2 <rini.angreani@curtin.edu.au>
2026-01-23 16:51:42 +0800

    Fix up sessions.logs call

--- automation/canfar_wrapper.py
+++ automation/canfar_wrapper.py
@@ -39,7 +39,7 @@ def run_canfar_task_with_polling(canfar_task, *args: None):
         if session_id:
             status = poll_canfar(session_id)     # keep polling until it stopped running        
             # print CANFAR logs before we lose them
-            logs_dict = session.logs(session_id, *args).get(session_id)
+            logs_dict = session.logs(session_id).get(session_id)
             print("CANFAR logs from session ", session_id, "\n", logs_dict)
 
             # check status and handle accordingly
